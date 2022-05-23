f2af3a43f4a5b7b29ca4542b77ba98f518701ef0
vinit9638 <106063233+vinit9638@users.noreply.github.com>
2022-05-23 16:14:45 +0530

    Add files via upload

---
+++ 23_4_search.py
@@ -0,0 +1,18 @@
+# database = ["Blockchain A to Z Explained: Become a Blockchain Pro with 400+ Terms","Blockchain: Blueprint for a New Economy","Inside the machine","Rich dad poor dad","shiva trilogy","hello dad","Core Python programming","The Psychology of Money","The Richest Man in Babylon"]
+
+# SEO 
+
+# database = [i.lower() for i in database]
+# query = input("enter your query :- ")
+# query = query.strip()
+# query = query.lower()
+# result = []
+# for i in database:
+#     if query in i:
+#         result.append(i)
+
+# if len(result) == 0:
+#     print("404 not found")
+# else:
+#     print(result)
+
---
+++ 23_5_methods_str.py
@@ -0,0 +1,85 @@
+# What is string ?? 
+# >> group of characters 
+# a = ''
+# a = ""
+# a = '''  '''
+# a = """  """
+# Immutable 
+
+
+# print(s1.capitalize())
+# print(s1.title())
+# s1 = s1.upper()
+# print(s1.lower())
+# print(s1.upper())
+# print(s1.swapcase())
+# print(s1)
+
+# print(s1.center(100,'-'))
+# print(s1.ljust(100,'-'))
+# print(s1.rjust(100,'-'))
+
+# print(s1.split())
+# l1 = s1.split('o')
+# print(l1)
+
+# print(s1.partition("Python"))
+# print(s1.strip())
+
+# s2 = (input("ENter your name:- "))
+# print(s2)
+# s2 = s2.strip()
+# if s2 == "vinit":
+#     print("hello")
+# else:
+#     print("drtfyguhi")
+# s2 = "     hello      "
+# print(s2.strip())
+
+
+
+# print(s1.startswith('he'))
+# print(s1.endswith('fun1'))
+
+# wed = input("Enter your website :- ")
+# wed = wed.lower()
+# if wed.count('o'): # True or False 
+#     print("Its an indian website")
+# elif wed.endswith(".ca"):
+#     print("Its an canadian website")
+
+# else:
+#     print("xyz")
+
+
+# print(s1.replace("o","a"))
+# print(s1)
+# s1 = "   hello    "
+# print(s1.rstrip())
+# print(s1.lstrip())
+# print(s1.strip())
+
+s1 = "hello world Python is Fun "
+
+# print(s1.count('O'))
+
+# print(s1.encode())
+# UTF - 8
+# print(s1.find('z'))
+# print(s1.index('z'))
+
+# name = "meet"
+# a = 34
+# b = 56
+
+# # print("My name is",name,"I have ",a," money ",b)
+# print(f"My name is {name} I have {a} money and {b}")
+
+# print("hello world {}, I have some money {}".format(name,a))
+
+# l1 = ["www","xyz","com"]
+
+# s2 = " ".join(l1)
+# print(s2)
+
+
